805d6e5efaf87a1b9d8348f6a44dbe3c03ee0519
SanginiERPReports <dsonani@sanginigroup.com>
2025-09-21 18:08:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,19 +7,8 @@ import xlsxwriter
 import streamlit as st
 
 col1, col2, col3 = st.columns([1, 2, 1])
-with col2:
-    st.image("SANGINI_LOGO.jpg", width=250)
-
-
-st.markdown(
-    """
-    <div style="display: flex; justify-content: center;">
-        <img src="SANGINI_LOGO.jpg" width="250"/>
-    </div>
-    """, 
-    unsafe_allow_html=True
-)
-
+with col1:
+    st.image("SANGINI_LOGO.jpg", width=350)
 
 
 st.set_page_config(page_title="PR–PO–GRN–GIN Linker", layout="wide")
